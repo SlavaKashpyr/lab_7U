535324b98c3b931cee8236bdeb467ac4e2d3b107
SlavaKashpyr <kashpyr2223@gmail.com>
2024-05-20 22:37:06 +0300

    Округление и добавление границ заметки

--- app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/NoteColor.kt
@@ -1,6 +1,8 @@
 package com.topic2.android.notes.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -8,13 +10,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun NoteColor() {
     Box(
-        modifier = Modifier.size(40.dp).background(Color.Red)
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .background(Color.Red)
+            .border(
+                BorderStroke(
+                    2.dp,
+                    SolidColor(Color.Black)
+                ),
+                CircleShape
+            )
     )
 }
 
